551471621116a1b0ebfaacaaeecc03eb28047fbb
second1996 <vital10012@gmail.com>
2020-03-23 02:59:47 +0200

    Update

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	var heroesSwiper = new Swiper ('.heroes__slider', {
+		// effect: 'fade',
+		allowTouchMove: false,
+		speed: 800,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
 		pagination: {
 			el: '.swiper-pagination',
+			clickable: true
 		},
-	})
+		preloadImages: false,
+		lazy: true,
+		lazy: {
+			loadPrevNext: true,
+		},
+	});
+
+	// $('img').on('dragstart', function(event) { event.preventDefault(); });
 
 });
